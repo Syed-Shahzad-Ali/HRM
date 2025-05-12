9fb2871477233caa1e0c25488aaf3b260992831e
Syed Shahzad Ali <alishahzad278@gmail.com>
2025-05-12 23:37:49 +0500

    Changes

--- hrm/settings.py
+++ hrm/settings.py
@@ -44,7 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'user_auth'
+    'user_auth',
+    'hrm_app'
 ]
 
 MIDDLEWARE = [
